83c67de0a827c28d2ce77032f9e14e190380926d
Timmy <afolayant537@gmail.com>
2026-04-27 00:44:34 +0100

    chore: implement validator utility function and add types for project

--- tests/unit/slug.test.ts
+++ tests/unit/slug.test.ts
@@ -2,13 +2,13 @@ import { describe, it, expect } from "vitest";
 import { getHabitSlug } from "@/lib/slug";
 
 describe("getHabitSlug", () => {
-  it("trims unnecessary spaces at the beginning and end for the habits name and collapses repeated internal spaces", () => {
+  it("trims outer spaces and collapses repeated internal spaces", () => {
     expect(getHabitSlug("   welcome   ")).toBe("welcome");
   });
-  it("returns a lowercase hyphenated string of the habit name in a case of more than one word", () => {
+  it("returns lowercase hyphenated slug for a basic habit name", () => {
     expect(getHabitSlug("Go to the Gym")).toBe("go-to-the-gym");
   });
-   it("Removes non alphanumeric characters except hyphens", () => {
+   it("removes non alphanumeric characters except hyphens ", () => {
     expect(getHabitSlug("Drink Water Today!!!!!!")).toBe("drink-water-today");
   });
 });
